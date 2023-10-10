531a48a4a124c17ec2233f51e88fd780c5a040ef
nikolas77380 <nkipniak@gmail.com>
2023-10-10 11:47:57 +0200

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
             © {year} WhatchIT. All Rights Reserved. All videos and shows on this
             platform are trademarks of, and all related images and content are
             the property of, Streamit Inc. Duplication and copy of this is
-            strictly prohibited. All rights reserved.
+            strictly prohibited. All rights reserved. I will add some text here! 
           </Box>
         </Grid>
       </Grid>
